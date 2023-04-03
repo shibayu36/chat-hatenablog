add3764542e0e8ebfe93c537353e3640a491c91c
shibayu36 <shibayu36@gmail.com>
2023-04-03 18:59:44 +0900

    Parse exported blog data

---
+++ make_index.py
@@ -0,0 +1,60 @@
+from collections import defaultdict
+import re
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+
+def parse_movable_type_entry(entry_text):
+    entry_data = defaultdict(lambda: None)
+    # Extract TITLE, BASENAME, BODY, EXTENDED BODY from MT format
+    title_pattern = re.compile(
+        r"^TITLE:\s*(.+)$",
+        re.MULTILINE
+    )
+    entry_data['title'] = title_pattern.search(entry_text).group(1)
+
+    basename_pattern = re.compile(
+        r"^BASENAME:\s*(.+)$",
+        re.MULTILINE
+    )
+    entry_data['basename'] = basename_pattern.search(entry_text).group(1)
+
+    body_pattern = re.compile(
+        r"^BODY:\s*(.+?)\n-----",
+        re.DOTALL | re.MULTILINE
+    )
+    entry_data['body'] = body_pattern.search(entry_text).group(1)
+
+    extended_body_pattern = re.compile(
+        r"^EXTENDED BODY:\s*(.+?)\n-----",
+        re.DOTALL | re.MULTILINE
+    )
+    extended_body_match = extended_body_pattern.search(entry_text)
+    if extended_body_match is not None:
+        entry_data['body'] += extended_body_match.group(1)
+
+    return entry_data
+
+
+def extract_entries_from_movable_type(movable_type_text):
+    entry_texts = re.split(r"(?m)^--------\n", movable_type_text)
+    entry_texts.pop()  # Delete last empty entry
+
+    entries = [parse_movable_type_entry(entry_text)
+               for entry_text in entry_texts]
+
+    return entries
+
+
+if __name__ == "__main__":
+    HATENABLOG_MT_FILE = os.getenv("HATENABLOG_MT_FILE")
+    INDEX_FILE = os.getenv("INDEX_FILE")
+
+    with open(HATENABLOG_MT_FILE, "r", encoding="utf-8") as file:
+        content = file.read()
+
+    entries = extract_entries_from_movable_type(content)
+    from pprint import pprint
+    pprint(entries)
