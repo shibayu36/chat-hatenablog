b6b0f1d5b5f83e2f74146eb7863449b351679bcf
shibayu36 <shibayu36@gmail.com>
2023-04-06 16:19:33 +0900

    Implement the feature to save embeddings to index.pickle

--- make_index.py
+++ make_index.py
@@ -3,9 +3,15 @@ import re
 import dotenv
 import html2text
 import argparse
+import pickle
 from langchain.text_splitter import MarkdownTextSplitter
 
 dotenv.load_dotenv()
+
+
+def create_embeddings(text, sleep_after_success=1):
+    # TODO: call embedding API
+    return [0.1, -0.1]
 
 
 def parse_movable_type_entry(entry_text):
@@ -82,15 +88,34 @@ def make_index_from_hatenablog(hatenablog_mt_file, index_file):
     entries = extract_entries_from_movable_type(content)
 
     markdown_splitter = MarkdownTextSplitter(chunk_size=1000, chunk_overlap=0)
+    vs = VectorStore(index_file)
 
     for entry in entries:
-        print(entry['title'])
-        print(entry['basename'])
         converted_body = convert_body_for_index(entry['body'])
         for chunk in markdown_splitter.split_text(converted_body):
-            print(chunk)
-            print('======')
-        print("--------")
+            vs.add_record(chunk, entry['title'], entry['basename'])
+
+        vs.save()
+
+
+class VectorStore:
+    def __init__(self, index_file):
+        self.index_file = index_file
+        try:
+            self.cache = pickle.load(open(self.index_file, "rb"))
+        except FileNotFoundError as e:
+            self.cache = {}
+
+    def add_record(self, body, title, basename):
+        if body not in self.cache:
+            # call embedding API
+            print('call embedding API')
+            self.cache[body] = (create_embeddings(body), title, basename)
+
+        return self.cache[body]
+
+    def save(self):
+        pickle.dump(self.cache, open(self.index_file, "wb"))
 
 
 def main(args):
@@ -102,7 +127,7 @@ if __name__ == "__main__":
         description="Create index from MT exported file.")
     parser.add_argument("--mt-file", required=True,
                         help="MT exported file path")
-    parser.add_argument("--index-file", default="index.txt",
+    parser.add_argument("--index-file", default="index.pickle",
                         help="Index file path")
 
     args = parser.parse_args()
