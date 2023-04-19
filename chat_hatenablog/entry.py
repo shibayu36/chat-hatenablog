6153f9a8339641ba73c9cbbbb6c715a8ddb7342b
shibayu36 <shibayu36@gmail.com>
2023-04-19 13:11:45 +0900

    Make content_hash

--- chat_hatenablog/entry.py
+++ chat_hatenablog/entry.py
@@ -1,5 +1,11 @@
+import hashlib
+
+
 class Entry:
     def __init__(self, title, body, basename):
         self.title = title
         self.body = body
         self.basename = basename
+
+    def content_hash(self):
+        return hashlib.sha256(self.body.encode('utf-8')).hexdigest()
