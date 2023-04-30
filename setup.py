a8dbf90012646148180a7794a5966db3ba4e751a
shibayu36 <shibayu36@gmail.com>
2023-04-30 14:12:06 +0900

    Make the repository able to be installed by pip

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="chat_hatenablog",
+    version="0.2.1",
+    author="Yuki Shibazaki",
+    author_email="shibayu36@gmail.com",
+    description="AI-powered software to interact with your HatenaBlog",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "python-dotenv==1.0.0",
+        "html2text==2020.1.16",
+        "langchain==0.0.132",
+        "tqdm==4.65.0",
+        "openai==0.27.4",
+        "tenacity==8.2.2",
+        "tiktoken==0.3.3",
+        "numpy==1.24.2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "chat-hatenablog = chat_hatenablog.main:main",
+        ]
+    },
+)
